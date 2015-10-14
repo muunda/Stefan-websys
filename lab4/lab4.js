1b04491d5893c8ec9d80287450028effc9b69c31
muunda <stefan.steenstrup@gmail.com>
2015-10-13 20:11:43 -0400

    Done
    It works!!!

--- lab4/lab4.js
+++ lab4/lab4.js
@@ -1,14 +1,15 @@
 $(document).ready(function() {
 	console.log("Ready!!!!");
 	$("#coverart").click(function() {
-  	alert( "Handler for .click() called." );
     $.ajax({ 
         type: 'GET', 
         url: 'lab4JSON.json',  
         dataType: 'json',
         success: function (data) { 
+        	$( "#site" ).remove();
+        	$('body').append('<div class = "song"><ul id="headings"><li class = "name">Song</li><li class = "artist">Artist</li><li class = "album">Album</li><li class = "art">Cover</li><li class = "date">Date</li><li class = "genre">Genre</li><li class = "link">Website</li></ul></div>');
          $.each(data.songs, function(index, element){
-         	var building = '<div class = "songs"><ul id="headings">'+ 
+         	var building = '<div class = "song"><ul>'+ 
          		'<li class = "name">'+ element.name+ '</li>'+
          		'<li class = "artist">'+ element.artist+ '</li>'+
          		'<li class = "album">'+ element.album+ '</li>'+ 
@@ -17,7 +18,6 @@ $(document).ready(function() {
          		'<li class = "genre">'+ element.genre+ '</li>'+ 
          		'<li class = "link">'+ element.link+ '</li>'+ 
          		'</ul></div>';
-         		alert(building);
          		$('body').append(building);
         });
     	}
